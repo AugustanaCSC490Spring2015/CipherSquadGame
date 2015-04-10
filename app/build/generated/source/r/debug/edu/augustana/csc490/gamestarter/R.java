aa26d4b890ea5b0647a9d2558e21d5bf6f6c71d1
Ethan Halsall <ethanhalsall11@augustana.edu>
2015-04-10 02:22:54 -0500

    Added icons and fixed descriptions

--- app/build/generated/source/r/debug/edu/augustana/csc490/gamestarter/R.java
+++ app/build/generated/source/r/debug/edu/augustana/csc490/gamestarter/R.java
@@ -11,43 +11,46 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000a;
-        public static final int algo1=0x7f090004;
-        public static final int algo2=0x7f090005;
-        public static final int algo3=0x7f090006;
-        public static final int gameStarterFragment=0x7f090008;
-        public static final int heightEditText=0x7f090001;
-        public static final int heightView=0x7f090007;
-        public static final int launchButton=0x7f090000;
-        public static final int mainGameView=0x7f090009;
-        public static final int widthEditText=0x7f090002;
-        public static final int widthView=0x7f090003;
+        public static final int action_settings=0x7f0a000a;
+        public static final int algo1=0x7f0a0004;
+        public static final int algo2=0x7f0a0005;
+        public static final int algo3=0x7f0a0006;
+        public static final int gameStarterFragment=0x7f0a0008;
+        public static final int heightEditText=0x7f0a0001;
+        public static final int heightView=0x7f0a0007;
+        public static final int launchButton=0x7f0a0000;
+        public static final int mainGameView=0x7f0a0009;
+        public static final int widthEditText=0x7f0a0002;
+        public static final int widthView=0x7f0a0003;
     }
     public static final class layout {
-        public static final int activity_intro=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int fragment_game=0x7f030002;
+        public static final int activity_intro=0x7f040000;
+        public static final int activity_main=0x7f040001;
+        public static final int fragment_game=0x7f040002;
     }
     public static final class menu {
-        public static final int menu_intro=0x7f080000;
+        public static final int menu_intro=0x7f090000;
+    }
+    public static final class mipmap {
+        public static final int ic_launcher=0x7f030000;
     }
     public static final class raw {
-        public static final int blocker_hit=0x7f040000;
-        public static final int cannon_fire=0x7f040001;
-        public static final int target_hit=0x7f040002;
+        public static final int blocker_hit=0x7f050000;
+        public static final int cannon_fire=0x7f050001;
+        public static final int target_hit=0x7f050002;
     }
     public static final class string {
-        public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int hello_world=0x7f060002;
-        public static final int title_activity_intro=0x7f060003;
+        public static final int action_settings=0x7f070000;
+        public static final int app_name=0x7f070001;
+        public static final int hello_world=0x7f070002;
+        public static final int title_activity_intro=0x7f070003;
     }
     public static final class style {
         /** 
@@ -58,9 +61,9 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
