d61776d7a2f2c3e7308083d0aaec96a8b9f02951
Jonathon Meyer <jonathonmeyer11@augustana.edu>
2015-05-08 08:45:37 -0500

    Changed the way that the initial difficulty is set

--- app/src/main/java/edu/augustana/csc490/ratrace/IntroActivity.java
+++ app/src/main/java/edu/augustana/csc490/ratrace/IntroActivity.java
@@ -9,18 +9,22 @@ import android.text.InputFilter;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.text.InputType;
 
 import edu.augustana.csc490.gamestarter.R;
 
 
 public class IntroActivity extends Activity {
 
-    int size; //initial maze size entered by user
+    int size = 5; //initial maze size entered by user
     EditText sizeEditText; //for user to enter the desired size for the initial maze
     Button launchButton;
     Button scoresButton;
+    ImageButton difficultyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,13 @@ public class IntroActivity extends Activity {
         setContentView(R.layout.activity_intro);
         launchButton = (Button) findViewById(R.id.launchButton);
         scoresButton = (Button) findViewById(R.id.scoresButton);
-        sizeEditText = (EditText) findViewById(R.id.sizeEditText);
+        //sizeEditText = (EditText) findViewById(R.id.sizeEditText);
+        difficultyButton =(ImageButton) findViewById(R.id.difficultyImageButton);
+
 
         launchButton.setOnClickListener(launchClickHandler);
         scoresButton.setOnClickListener(scoresClickHandler);
-
+        difficultyButton.setOnClickListener(difficultyClickHandler);
     }
 
     // click handler used to launch the game when launchButton is clicked
@@ -58,7 +64,7 @@ public class IntroActivity extends Activity {
                 public void onClick(DialogInterface dialog, int whichButton) {
                     String value = input.getText().toString();
                     Intent intent = new Intent(IntroActivity.this, MainActivity.class);
-                    size = Integer.parseInt(sizeEditText.getText().toString());
+
                     intent.putExtra("size", size);
                     intent.putExtra("initials", value);
                     startActivity(intent);
@@ -89,7 +95,44 @@ public class IntroActivity extends Activity {
         }
     };
 
-    @Override
+    View.OnClickListener difficultyClickHandler = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            final AlertDialog.Builder alert = new AlertDialog.Builder(IntroActivity.this);
+
+            alert.setTitle("Set Starting Difficulty");
+
+            // Set an EditText view to get user input
+            //Also set the edit text to only accept 3 characters
+            final EditText input = new EditText(IntroActivity.this);
+            input.setText(size+"");
+            int maxLength = 3;
+            InputFilter[] FilterArray = new InputFilter[1];
+            FilterArray[0] = new InputFilter.LengthFilter(maxLength);
+            input.setInputType(InputType.TYPE_CLASS_NUMBER);
+            input.setFilters(FilterArray);
+            alert.setView(input);
+
+            //alert.setView(input);
+
+            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    size = Integer.parseInt(input.getText().toString());
+
+
+                }
+            });
+
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+
+                }
+            });
+
+            alert.show();
+        }
+    };
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_intro, menu);
