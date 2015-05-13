41286473c7258dfee2caac0c8f621e6c0a881a63
Ethan Halsall <ethanhalsall11@augustana.edu>
2015-05-12 22:49:05 -0500

    Fixed the issue of very large mazes not filling the screen. The MazeLineArray class now uses floating point division instead of integer division and then uses Math.round to change these floating point values into integers which correspond to pixels on the screen. Impossibly large mazes now display correctly!

--- app/src/main/java/edu/augustana/csc490/ratrace/maze/MazeLineArray.java
+++ app/src/main/java/edu/augustana/csc490/ratrace/maze/MazeLineArray.java
@@ -1,6 +1,7 @@
 package edu.augustana.csc490.ratrace.maze;
 
 import android.graphics.Point;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -13,44 +14,32 @@ public class MazeLineArray {
     //stores information for the maze line array
     private Maze maze;
     private ArrayList<Line> mazeLineArray;
-    private int screenWidth;
-    private int screenHeight;
-    private int width;
-    private int height;
+    private float screenWidth;
+    private float screenHeight;
+    private float width;
+    private float height;
     private boolean[] hWalls;
     private boolean[] vWalls;
-    private int wSpacing;
-    private int hSpacing;
 
 
     //uses the screen dimensions to scale the size of the lines
     public MazeLineArray(Maze maze, int screenWidth, int screenHeight) {
         this.maze = maze;
-        this.screenHeight = screenHeight;
-        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight - 1;
+        this.screenWidth = screenWidth - 1;
         width = maze.getWidth();
         height = maze.getHeight();
         hWalls = maze.getHWalls();
         vWalls = maze.getVWalls();
-        wSpacing = screenWidth / width;
-        hSpacing = screenHeight / height;
         createMazeLineArray();
     }
 
     public int getScreenWidth() {
-        return screenWidth;
+        return (int) screenWidth;
     }
 
     public int getScreenHeight() {
-        return screenHeight;
-    }
-
-    public int getWSpacing() {
-        return wSpacing;
-    }
-
-    public int getHSpacing() {
-        return hSpacing;
+        return (int) screenHeight;
     }
 
     public Line getLineAtIndex(int index) {
@@ -65,47 +54,73 @@ public class MazeLineArray {
         return maze;
     }
 
-    public Point screenToMazePos(int screenX, int screenY) {
-        int mazeX = screenX / (screenWidth + wSpacing / 2);
-        int mazeY = screenY / (screenHeight + hSpacing / 2);
-        return new Point(mazeX, mazeY);
+    public int getWSpacing() {
+        /*double widthSpacing = 0;
+        double i;
+        for (i = 1; i <= width; i++){
+            widthSpacing += (screenWidth * (i / width));
+        }
+        widthSpacing = widthSpacing / i;
+        return (int) widthSpacing;*/
+        return (int) (screenWidth / width);
+    }
+
+    public int getHSpacing() {
+        return (int) (screenHeight / height);
     }
 
     private void createMazeLineArray() {
         mazeLineArray = new ArrayList<Line>();
 
         //iterates through the maze boolean arrays to create an array of lines
-        int currentH;
-        int wCursorPos = 0;
+        int wCursorPosLeft;
+        int wCursorPosRight;
+        int hCursorPosTop;
+        int hCursorPosBottom;
+        float wall;
+
         for (int i = 0; i < height; i++) {
-            currentH = i * hSpacing;
+            //currentH = i * hSpacing;
+            hCursorPosTop = Math.round(screenHeight * (i / height));
+            hCursorPosBottom = Math.round(screenHeight * ((i + 1) / height));
 
             //draws one set of horizontal walls
-            for (int h = i * width; h < (i + 1) * width; h++) {
+            wall = 0;
+            for (int h = i * (int) width; h < (i + 1) * width; h++) {
+
+                wCursorPosLeft = Math.round(screenWidth * (wall / width));
+                wCursorPosRight = Math.round(screenWidth * ((wall + 1) / width));
                 if (hWalls[h]) {
-                    mazeLineArray.add(new Line(wCursorPos, currentH, wCursorPos + wSpacing, currentH));
+                    mazeLineArray.add(new Line(wCursorPosLeft, hCursorPosTop, wCursorPosRight, hCursorPosTop));
                 }
-                wCursorPos += wSpacing;
+                wall++;
+
             }
 
             //draws one set of vertical walls
-            wCursorPos = 0;
+            wall = 0;
             for (int v = i; v <= height * width + i; v += height) {
+
+                wCursorPosLeft = Math.round(screenWidth * (wall / width));
+                wall++;
+
                 if (vWalls[v]) {
-                    mazeLineArray.add(new Line(wCursorPos, currentH, wCursorPos, currentH + hSpacing));
+                    mazeLineArray.add(new Line(wCursorPosLeft, hCursorPosTop, wCursorPosLeft, hCursorPosBottom));
                 }
-                wCursorPos += wSpacing;
             }
-            wCursorPos = 0;
         }
 
         //draws the last set of horizontal walls
-        currentH = screenHeight;
-        for (int h = width * width; h < (width + 1) * width; h++) {
+        hCursorPosTop = (int) screenHeight;
+        wall = 0;
+        for (int h = (int) width * (int) width; h < (width + 1) * width; h++) {
+            wCursorPosLeft = Math.round(screenWidth * (wall / width));
+            wCursorPosRight = Math.round(screenWidth * ((wall + 1) / width));
+
             if (hWalls[h]) {
-                mazeLineArray.add(new Line(wCursorPos, currentH, wCursorPos + wSpacing, currentH));
+                mazeLineArray.add(new Line(wCursorPosLeft, hCursorPosTop, wCursorPosRight, hCursorPosTop));
             }
-            wCursorPos += wSpacing;
+            wall++;
         }
 
     }
